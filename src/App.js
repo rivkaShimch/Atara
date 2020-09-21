86562050ff1f395f3674b2b5d21a9a69da106ac2
rivkaShimch <33696268+rivkaShimch@users.noreply.github.com>
2020-09-21 20:58:29 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-function App() {
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import NavBar from "./components/navbar";
+import CreateUser from "./components/create-user";
+import About from "./components/about";
+import Home from "./components/Home/home";
+import Login from './components/Login/login';
+import Empty_page from './components/Empty_page/empty_page';
+
+
+const App = function () {
   return (
     <Router>
       <div className="container">
