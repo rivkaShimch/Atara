63867f6412846b52b03b7df0d55359cec9a3b981
rivkaShimch <33696268+rivkaShimch@users.noreply.github.com>
2020-10-12 11:51:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-function App() {
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import axios from 'axios';
